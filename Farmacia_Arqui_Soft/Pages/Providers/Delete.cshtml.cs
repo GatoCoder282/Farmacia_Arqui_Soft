492b03f8c70e0e152501b21643348b77165ee13b
Eduardo Marin Espinoza <emarine@Eduardos-Laptop.local>
2025-10-16 08:54:38 -0400

    Delete refactor provideService Instance

--- Farmacia_Arqui_Soft/Pages/Providers/Delete.cshtml.cs
+++ Farmacia_Arqui_Soft/Pages/Providers/Delete.cshtml.cs
@@ -1,45 +1,23 @@
 using System.Threading.Tasks;
-using Farmacia_Arqui_Soft.Domain.Models;
-using Farmacia_Arqui_Soft.Domain.Ports;
+using Farmacia_Arqui_Soft.Application.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Farmacia_Arqui_Soft.Infraestructure.Persistence;
 
 namespace Farmacia_Arqui_Soft.Pages.Providers
 {
     public class DeleteModel : PageModel
     {
-        private readonly IRepository<Provider> _repo;
+        private readonly ProviderService _service;
 
-        public DeleteModel()
+        public DeleteModel(ProviderService service)
         {
-            var factory = new ProviderRepositoryFactory();
-            _repo = factory.CreateRepository<Provider>();
+            _service = service;
         }
 
-        [BindProperty]
-        public Provider Provider { get; set; } = new Provider();
-
-
-        public async Task<IActionResult> OnGetAsync(int id)
+        public async Task<IActionResult> OnPostAsync(int id)
         {
-            var tempProvider = new Provider { id = id };
-            var userFromDb = await _repo.GetById(tempProvider);
-
-            if (userFromDb == null)
-            {
-                return RedirectToPage("Index");
-            }
-
-            Provider = userFromDb;
-            return Page();
-        }
-
-        public async Task<IActionResult> OnPostAsync()
-        {
-            await _repo.Delete(Provider);
+            await _service.DeleteAsync(id);
             return RedirectToPage("Index");
         }
-        
     }
 }
